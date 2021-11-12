37d640409ca9ad671257f52fb9c7904a466bac06
tiwuofficial <theimaginationwasunbroken@gmail.com>
2021-11-12 15:59:49 +0900

    use wasm from dom

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 import init, {greet2} from "/pkg/wasm_pack_sample.js";
 init().then(() => {
   window.greet2 = greet2;
-  console.log('load');
-  console.log(window.greet2('hello! from js'));
 });
   
+window.addEventListener("load", () => {
+  document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("result").textContent = window.greet2(document.getElementById("text").value);
+  });
+});
